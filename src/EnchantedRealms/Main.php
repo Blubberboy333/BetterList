2427cb6a354e68c61d4dccbf47cc22dcfa763c33
Joe [ERPE] [SA] <JoeycraftPE@gmail.com>
2016-06-06 20:30:23 -0500

    prob lots of errors its too late at night XD

--- src/EnchantedRealms/Main.php
+++ src/EnchantedRealms/Main.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace Main;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+	      public function onEnable(){
+		            $this->getLogger()->info(C::YELLOW . "Enabled BetterList!");
+	 }
+	
+   public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+            if(strtolower($cmd->getName()) == "list"){
+              $online = ???
+              $maxplayers = ???
+                $sender->sendMessage("§");
+                $sender->sendMessage("§aPlayer Count§c:§b $online §3/§b $maxplayers!);
